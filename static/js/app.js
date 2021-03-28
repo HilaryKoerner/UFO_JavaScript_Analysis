158f29d368e5060502789bc9a54c4728f896d83e
Hilary <hilary.koerner@gmail.com>
2021-03-28 14:25:04 -0500

    fixed filter and parsed date

--- static/js/app.js
+++ static/js/app.js
@@ -1,96 +1,40 @@
 // from data.js
 var ufoData = data;
-
 //select the button and input
 var button = d3.select("#filter-btn")
 var input = d3.select("#datetime")
-
 //event listeners
 button.on("click", displayData);
 input.on("change", displayData);
-
 //complete the event handler function for the form
 function displayData() {
   //select the input and get the raw HTML node
   var inputElement = d3.select("#datetime");
   //get the value of the user input element
   var inputValue = inputElement.property("value");
+  // var filtered = [];
+  // // console.log(ufoData);
+  // ufoData.forEach(function (d) {
+  //   if(Date.parse(d.datetime) == Date.parse(inputValue)) {
+  //     filtered.push(d);
+  //   }
+  // });
+  // console.log(filtered);
   // console.log(inputValue);
   // console.log(ufoData);
-
-  var filteredDate = ufoData.filter(userDate => userDate.datetime === inputValue);
-
-  console.log(typeof(ufoData[0].datetime));}
-  
-  var city = filteredDate.map(userDate => userDate);
-  
-  consolelog(unfo);
-
+  // console.log(ufoData[0].datetime)
+  var filteredDate = ufoData.filter(d => Date.parse(d.datetime) === Date.parse(inputValue));
+  // console.log("hello");
+  // console.log(filteredDate);
+  //var city = filteredDate.map(userDate => userDate);
+  //consolelog(unfo);
   var tbody = d3.select("tbody");
-  data.forEach(filteredDate => {
+  tbody.html("")
+  filteredDate.forEach(d => {
     var row = tbody.append('tr')
-    Object.entries(filteredDate).forEach(function([key, value]) {
+    Object.entries(d).forEach(function ([key, value]) {
       var cell = row.append('td')
       cell.text(value)
-    })
-  });
-
-
-
-
-// // // // Get a reference to the table body
-// // var tbody = d3.select("tbody");
-
-// // // // Console.log the UFO data from data.js
-// // console.log(data);
-
-
-
-// // function displayData() {
-// //   data.forEach(ufoSightings => {
-// //     var row = tbody.append('tr')
-// //     Object.entries(ufoSightings).forEach(function([key, value]) {
-// //       var cell = row.append('td')
-// //       cell.text(value)
-// //     })
-// //   })
-// // }
-
-
-
-// // //input and output elements
-// // var date = d3.select("#datetime");
-// // var output = d3.select("#ufo-table");
-
-// // //event listener (to grab date from above)
-// // data.forEach(ufoSightings => {
-// //   var row = tbody.append('tr')
-// //   Object.entries(ufoSightings).forEach(function([key, value]) {
-// //     var cell = row.append('td')
-// //     cell.text(value)
-// //   })
-// // })
-
-// // //event handler
-
-// // //this will clear the table (not yet)
-// // output.table("")
-
-
-
-
-
-
-
-// // // // // UFO report info (Date, City, State, Country, Shape, Duration, Comments)
-// // data.forEach(function(ufoSightings) {
-// //   // console.log(ufoSightings);
-// //   var row = tbody.append("tr");
-// //   Object.entries(ufoSightings).forEach(function([key, value]) {
-// //     // console.log(key, value);
-// //     // Append a cell to the row for each value
-// //     // in the weather report object
-// //     var cell = row.append("td");
-// //     cell.text(value);
-// //   });
-// // });
+    });
+  })
+};
